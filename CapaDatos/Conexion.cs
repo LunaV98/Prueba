0abd72a01a6572cc70f29a3905522230bdc93512
LunaV98 <71519914+LunaV98@users.noreply.github.com>
2020-10-03 12:51:10 -0500

    Add files via upload

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source=DESKTOP-6JVSUBD;Initial Catalog=dbventas;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
